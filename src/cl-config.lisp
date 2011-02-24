4488f6b2481a19356f24432acda4affe4400c850
Mariano Montone <mariano@copyleft.no>
2011-02-24 14:18:48 -0300

    Little bug fix

--- src/cl-config.lisp
+++ src/cl-config.lisp
@@ -1,6 +1,7 @@
 (in-package :cl-config)
 
 (defvar *configuration* nil "The current configuration")
+(defvar *configurations* (make-hash-table :test #'equalp))
 
 (defun read-configuration-option (path &optional (configuration *configuration*))
   )
