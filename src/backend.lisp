f82e6420be1f0440cc10e1f8a3c62af811ec2f06
Mariano Montone <mariano@copyleft.no>
2011-02-26 23:15:05 -0300

    *input-backend*

--- src/backend.lisp
+++ src/backend.lisp
@@ -4,6 +4,7 @@
   ())
 
 (defparameter *output-backend* nil)
+(defparameter *input-backend* nil)
 
 (defun serialize (configuration stream &optional (output-backend *output-backend*))
   (run output-backend configuration stream))
