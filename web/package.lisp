2dcafa51aa7d46da2f7e274aa17f14714116a3b3
Mariano Montone <mariano@copyleft.no>
2011-03-28 23:07:35 -0300

    [Web] Exporting start/stop functions

--- web/package.lisp
+++ web/package.lisp
@@ -1,4 +1,5 @@
 (defpackage cl-config.web
   (:nicknames :cfg.web)
-  (:use :cl :cl-config :hunchentoot :cl-who :parenscript))
-
+  (:use :cl :cl-config :hunchentoot :cl-who :parenscript)
+  (:export #:start-cl-config-web
+	   #:stop-cl-config-web))
