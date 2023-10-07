1beccd9aa29e876040c735c8c4c141a7c378a32c
Mariano Montone <marianomontone@gmail.com>
2023-10-07 11:30:11 -0300

    Utils

---
+++ corelib/util.scm
@@ -0,0 +1,40 @@
+(require 'regex)
+(require 'eval)
+(require 'srfi-1)
+
+(define proc-regex "#<primitive-procedure (.*)>")
+
+(define (proc-name proc)
+  (let* ((printed-proc (call-with-output-string
+                        (lambda (out)
+                          (write proc out))))
+         (matchv (regmatchv proc-regex printed-proc)))
+    (if matchv
+        (substring printed-proc
+                   (vector-ref matchv 2)
+                   (vector-ref matchv 3))
+        #f)))
+
+(define apropos-defs '())
+
+;; (define (init-apropos-defs)
+;;   (set! apropos-defs '())
+;;   (for-each (lambda (proc)
+;;               (let ((pname (proc-name proc)))
+;;                 (if pname
+;;                     (set! apropos-defs (cons pname apropos-defs)))))
+;;             (cadr (scheme-report-environment 5)))
+;;   apropos-defs)
+
+(define (init-apropos-defs)
+  (set! apropos-defs (cadr (scheme-report-environment 5))))
+
+(init-apropos-defs)
+
+(define (apropos what)
+  (cond
+   ((symbol? what)
+    (apropos (symbol->string what)))
+   ((string? what)
+    (filter (lambda (sym) (regsearch what (symbol->string sym)))
+            apropos-defs))))
