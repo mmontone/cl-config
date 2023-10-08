eceede6b3d91fb4682ceaf2b2de47281e912b59c
Mariano Montone <marianomontone@gmail.com>
2023-10-08 17:15:51 -0300

    Move to util

--- corelib/util.scm
+++ corelib/util.scm
@@ -2,6 +2,95 @@
 (require 'eval)
 (require 'srfi-1)
 
+(require 'format)
+
+(define (empty? list)
+  (zero? (length list)))
+
+(define (assert condition . message-and-args)
+  (let ((message (if (empty? message-and-args)
+                     "Assertion failed"
+                     (car message-and-args)))
+        (args (if (empty? message-and-args)
+                  '()
+                  (cdr message-and-args))))
+    (if (not condition)
+        (error (apply format message args)))))
+
+;; (assert #t)
+;; (assert #f)
+;; (assert #f "No")
+;; (assert #f "No: ~a" 'foo)
+
+(define (car* lst)
+  (and (not (empty? lst))
+       (car lst)))
+
+(car* '())
+(car* '(a))
+
+(define (get-prop key property-list . default)
+  (cond ((null? property-list) (if (not (null? default))
+                                   (car default)
+                                   #f))
+        ((null? (cdr property-list))
+         (error "Malformed property list"))
+        ((equal? key (car property-list))
+         (cadr property-list))
+        (else (apply get-prop key (cons (cdr (cdr property-list)) default)))))
+
+(define (call-with-optional-args args defaults proc)
+  (if (> (length args) (length defaults))
+      (error "Bad arguments"))
+  (let ((i 0))
+    (apply proc
+           (map (lambda (default)
+                  (let ((val
+                         (if (<= (1+ i) (length args))
+                             (list-ref args i)
+                             default)))
+                    (set! i (1+ i))
+                    val))
+                defaults))))
+
+;;(call-with-optional-args '() '(#t #f)
+;;                         (lambda (x y) (list x y)))
+
+;;(call-with-optional-args '(foo) '(#t #f)
+;;                         (lambda (x y) (list x y)))
+
+;;(call-with-optional-args '(foo bar) '(#t #f)
+;;                         (lambda (x y) (list x y)))
+
+;;(call-with-optional-args '(foo bar baz) '(#t #f)
+;;                         (lambda (x y) (list x y)))
+
+(define (assoc-set! lst key value)
+  (let ((ass (assoc key lst)))
+    (if ass
+        (begin (set-cdr! ass value)
+               lst)
+        (append lst (list (cons key value))))))
+
+(define (assoc-get lst key . default)
+  (let ((pair (assoc key lst)))
+    (if (pair? pair)
+        (values (cdr pair) #t)
+        (values (if (and (not (null? default))
+                         (procedure? (car default)))
+                    ((car default))
+                    #f)
+                #f))))
+
+(define (assoc-get* lst key . default)
+  (call-with-values
+      (lambda () (apply assoc-get lst (cons key default)))
+    (lambda (val found?)
+      val)))
+
+;;(assoc-get* '() 'x)
+;;(assoc-get* '((x . 22)) 'x)
+
 (define proc-regex "#<primitive-procedure (.*)>")
 
 (define (proc-name proc)
