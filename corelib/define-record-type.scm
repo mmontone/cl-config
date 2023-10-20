589d4b63273d8ef7fd331a2018ee0da444871245
Mariano Montone <marianomontone@gmail.com>
2023-10-20 10:55:10 -0300

    Scheme stuff

--- corelib/define-record-type.scm
+++ corelib/define-record-type.scm
@@ -1,47 +1,4 @@
-(define (list-index fn list)
-  (let iter ((list list) (index 0))
-    (if (null? list)
-        -1
-        (let ((item (car list)))
-          (if (fn item)
-              index
-              (iter (cdr list) (+ index 1)))))))
-
-(define (make-record name fields)
-  (let ((record (make-vector 4)))
-    (vector-set! record 0 'record) ;; tag
-    (vector-set! record 1 name) ;; name
-    (vector-set! record 2 fields) ;; field names
-    (vector-set! record 3 (make-vector (length fields))) ;; field values
-    record))
-
-(define (curry f . c)
-  (lambda x (apply f (append c x))))
-
-(define (field-value record field-name)
-  (let ((field-index (list-index (curry eqv? field-name)
-                                 (vector-ref record 2))))
-    (if (=? field-index -1)
-        (error "Invalid field")
-        (vector-ref (vector-ref record 3) field-index))))
-
-(define (set-field-value! record field-name value)
-  (let
-      ((field-index (list-index (curry eqv? field-name)
-                                (vector-ref record 2))))
-    (if (=? field-index -1)
-        (error "Invalid field")
-        (begin
-          (vector-set! (vector-ref record 3) field-index value)
-          value))))
-
-;;(define myrec
-;;  (make-record 'my-record '(name lastname)))
-
-;; (field-value myrec 'name)
-;; (set-field-value! myrec 'name "Mariano")
-;; (field-value myrec 'name)
-;; (set-field-value! myrec 'name "Martin")
+(load "record.scm")
 
 (define (expand-define-record-type name options)
   (let* ((constructor (list-ref options 0))
--- corelib/util.scm
+++ corelib/util.scm
@@ -1,3 +1,5 @@
+;;(define compile-all-proc-redefined #t)
+
 (require 'regex)
 (require 'eval)
 (require 'srfi-1)
@@ -87,43 +89,6 @@
 
 ;;(assoc-get* '() 'x)
 ;;(assoc-get* '((x . 22)) 'x)
-
-(define proc-regex "#<primitive-procedure (.*)>")
-
-(define (proc-name proc)
-  (let* ((printed-proc (call-with-output-string
-                        (lambda (out)
-                          (write proc out))))
-         (matchv (regmatchv proc-regex printed-proc)))
-    (if matchv
-        (substring printed-proc
-                   (vector-ref matchv 2)
-                   (vector-ref matchv 3))
-        #f)))
-
-(define apropos-defs '())
-
-;; (define (init-apropos-defs)
-;;   (set! apropos-defs '())
-;;   (for-each (lambda (proc)
-;;               (let ((pname (proc-name proc)))
-;;                 (if pname
-;;                     (set! apropos-defs (cons pname apropos-defs)))))
-;;             (cadr (scheme-report-environment 5)))
-;;   apropos-defs)
-
-(define (init-apropos-defs)
-  (set! apropos-defs (cadr (scheme-report-environment 5))))
-
-(init-apropos-defs)
-
-(define (apropos what)
-  (cond
-   ((symbol? what)
-    (apropos (symbol->string what)))
-   ((string? what)
-    (filter (lambda (sym) (regsearch what (symbol->string sym)))
-            apropos-defs))))
 
 ;; https://pschombe.wordpress.com/2006/03/10/destructuring-bind/
 ;; (define-syntax destructuring-bind
